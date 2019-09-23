a2f7d102410e2519f71a20b28df682b7237ca3e8
Ahmad Arefin Kawsar <54704888+noobCoder9304@users.noreply.github.com>
2019-09-23 20:35:19 +0600

    Add files via upload

---
+++ ebook.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+using namespace std;
+class story{
+public:
+    char x;
+    void getstory()
+    {
+        cout<<"               Read Amazing Short Stories!!"<<endl<<endl<<endl<<endl;
+        cout<<"Our Library:- "<<endl<<endl;
+        cout<<"a) Rapunzel."<<endl;
+        cout<<"b) The Little Match Seller"<<endl;
+        cout<<"c) Thumbelina"<<endl;
+        cout<<"d) The princess and the Pea"<<endl<<endl;
+        cout<<"Which one? : ";
+
+        cin>>x;
+        switch(x)
+        {
+    case'd':
+        cout<<"                        The Princess And the Pea"<<endl;
+        cout<<"Once upon a time there was a prince who wanted to marry a princess; but she would have to be a real princess."<<endl;
+        cout<<"He travelled all over the world to find one, but nowhere could he get what he wanted. There were princesses enough, but it was difficult to find out whether they were real ones."<<endl;
+        cout<<"There was always something about them that was not as it should be. So he came home again and was sad, for he would have liked very much to have a real princess."<<endl;
+        cout<<" One evening a terrible storm came on; there was thunder and lightning, and the rain poured down in torrents. Suddenly a knocking was heard at the city gate, and the old king went to open it."<<endl;
+        cout<<" It was a princess standing out there in front of the gate. But, good gracious! what a sight the rain and the wind had made her look. The water ran down from her hair and clothes;"<<endl;
+        cout<<"it ran down into the toes of her shoes and out again at the heels. And yet she said that she was a real princess."<<endl;
+        cout<<"Well, we'll soon find that out, thought the old queen. But she said nothing, went into the bed-room, took all the bedding off the bedstead,"<<endl;
+        cout<<"and laid a pea on the bottom; then she took twenty mattresses and laid them on the pea, and then twenty eider-down beds on top of the mattresses."<<endl;
+        cout<<"On this the princess had to lie all night. In the morning she was asked how she had slept."<<endl;
+        cout<<"Oh, very badly! said she. I have scarcely closed my eyes all night. Heaven only knows what was in the bed,"<<endl;
+        cout<<"but I was lying on something hard, so that I am black and blue all over my body. It's horrible!"<<endl;
+        cout<<" Now they knew that she was a real princess because she had felt the pea right through the twenty mattresses and the twenty eider-down beds."<<endl<<endl;
+        cout<<"                                            The END"<<endl;
+        break;
+    case'c':
+        cout<<"                                   Thumbelina"<<endl;
+        cout<<""<<endl;
+        cout<<"There was once a woman who wished very much to have a little child, but she could not obtain her wish. At last she went to a fairy, and said,I should so very much like to have a little child; can you tell me where I can Find one?"<<endl;
+        cout<<"Oh, that can be easily managed,said the fairy.Here is a barleycorn of a different kind to those which grow in the farmer's fields, and which the chickens eat; put it into a flower-pot, and see what will happen."<<endl;
+        cout<<"Thank you,said the woman, and she gave the fairy twelve shillings, which was the price of the barleycorn. Then she went home and planted it, and immediately there grew up a large handsome flower, something like a tulip in appearance, but with its leaves tightly closed as if it were still a bud."<<endl;
+        cout<<"It is a beautiful flower,said the woman, and she kissed the red and golden-colored leaves, and while she did so the flower opened, and she could see that it was a real tulip. Within the flower, upon the green velvet stamens, sat a very delicate and graceful little maiden."<<endl;
+        cout<<"She was scarcely half as long as a thumb, and they gave her the name of Thumbelina,or Tiny, because she was so small. A walnut-shell, elegantly polished, served her for a cradle;"<<endl;
+        cout<<" her bed was formed of blue violet-leaves, with a rose-leaf for a counterpane. Here she slept at night, but during the day she amused herself on a table,"<<endl;
+        cout<<"where the woman had placed a plateful of water. Round this plate were wreaths of flowers with their stems in the water, and upon it floated a large tulip-leaf,"<<endl;
+        cout<<"which served Tiny for a boat. Here the little maiden sat and rowed herself from side to side, with two oars made of white horse-hair. It really was a very pretty sight. Tiny could, also, sing so softly and sweetly that nothing like her singing had ever before been heard."<<endl;
+        cout<<"One night, while she lay in her pretty bed, a large, ugly, wet toad crept through a broken pane of glass in the window, and leaped right upon the table where Tiny lay sleeping under her rose-leaf quilt."<<endl;
+        cout<<"What a pretty little wife this would make for my son,said the toad, and she took up the walnut-shell in which little Tiny lay asleep, and jumped through the window with it into the garden."<<endl;
+        cout<<"                                          The end"<<endl<<endl;
+        break;
+    case'b':
+        cout<<"                         The little Match seller"<<endl;
+        cout<<"It was terribly cold and nearly dark on the last evening of the old year, and the snow was falling fast. In the cold and the darkness, a poor little girl, with bare head and naked feet,"<<endl;
+        cout<<"roamed through the streets. It is true she had on a pair of slippers when she left home, but they were not of much use. They were very large, so large,"<<endl;
+        cout<<"indeed, that they had belonged to her mother, and the poor little creature had lost them in running across the street to avoid two carriages that were rolling along at a terrible rate."<<endl;
+        cout<<"One of the slippers she could not find, and a boy seized upon the other and ran away with it, saying that he could use it as a cradle, when he had children of his own."<<endl;
+        cout<<"So the little girl went on with her little naked feet, which were quite red and blue with the cold."<<endl;
+        cout<<"In an old apron she carried a number of matches, and had a bundle of them in her hands. No one had bought anything of her the whole day, nor had any one given here even a penny."<<endl;
+        cout<<"Shivering with cold and hunger, she crept along; poor little child, she looked the picture of misery."<<endl;
+        cout<<"The snowflakes fell on her long, fair hair, which hung in curls on her shoulders, but she regarded them not."<<endl;
+        cout<<" Lights were shining from every window, and there was a savory smell of roast goose, for it was New-year's eve - yes, she remembered that. In a corner, between two houses, one of which projected beyond the other,"<<endl;
+        cout<<"she sank down and huddled herself together. She had drawn her little feet under her, but she could not keep off the cold; and she dared not go home,"<<endl;
+        cout<<"for she had sold no matches, and could not take home even a penny of money. Her father would certainly beat her; besides, it was almost as cold at home as here,"<<endl;
+        cout<<"for they had only the roof to cover them, through which the wind howled, although the largest holes had been stopped up with straw and rags."<<endl;
+        cout<<"Her little hands were almost frozen with the cold. Ah! perhaps a burning match might be some good, if she could draw it from the bundle and strike it against the wall, just to warm her fingers."<<endl;
+        cout<<"                                  The End"<<endl;
+        break;
+    case'a':
+        cout<<"                               Rapunzel "<<endl;
+        cout<<"There were once a man and a woman who had long, in vain, wished for a child. At length it appeared that God was about to grant their desire."<<endl;
+        cout<<"These people had a little window at the back of their house from which a splendid garden could be seen, which was full of the most beautiful flowers and herbs."<<endl;
+        cout<<"It was, however, surrounded by a high wall, and no one dared to go into it because it belonged to an enchantress, who had great power and was dreaded by all the world."<<endl;
+        cout<<" One day the woman was standing by this window and looking down into the garden, when she saw a bed which was planted with the most beautiful rampion,"<<endl;
+        cout<<"and it looked so fresh and green that she longed for it. She quite pined away, and began to look pale and miserable. Her husband was alarmed, and asked: 'What ails you, dear wife?'"<<endl;
+        cout<<" 'Ah,' she replied, 'if I can't eat some of the rampion, which is in the garden behind our house, I shall die.'"<<endl;
+        cout<<"The man, who loved her, thought: 'Sooner than let your wife die, bring her some of the rampion yourself, let it cost what it will."<<endl;
+        cout<<" At twilight, he clambered down over the wall into the garden of the enchantress, hastily clutched a handful of rampion, and took it to his wife"<<endl;
+        cout<<"She at once made herself a salad of it, and ate it greedily. It tasted so good to her - so very good, that the next day she longed for it three times as much as before."<<endl;
+        cout<<" If he was to have any rest, her husband knew he must once more descend into the garden. Therefore, in the gloom of evening, "<<endl;
+        cout<<"he let himself down again; but when he had clambered down the wall he was terribly afraid, for he saw the enchantress standing before him."<<endl;
+        cout<<" 'How can you dare,' said she with angry look, 'descend into my garden and steal my rampion like a thief? You shall suffer for it!'"<<endl;
+        cout<<"                                       The END"<<endl;
+        break;
+    default:
+        cout<<" We have only Stories About a to b....please chose among them"<<endl;
+        }
+    }
+};
+int main()
+{
+int n;
+
+cout<<"Wanna Read Story?...press 1: ";
+cin>>n;
+while(n==1)
+{
+    story s1;
+    s1.getstory();
+    cout<<"Want to continue reading? press 1: ";
+    cin>>n;
+}
+return 0;
+}
